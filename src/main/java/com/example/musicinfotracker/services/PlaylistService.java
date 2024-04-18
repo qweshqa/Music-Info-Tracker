a699b6b576894114b2dbf2bdd694b6d871c3f002
qweshqa <stasigrok2712@gmail.com>
2024-04-18 23:12:35 +0300

    add playlist tracks to the playlist page

--- src/main/java/com/example/musicinfotracker/services/PlaylistService.java
+++ src/main/java/com/example/musicinfotracker/services/PlaylistService.java
@@ -1,6 +1,8 @@
 package com.example.musicinfotracker.services;
 
+import com.example.musicinfotracker.dto.Artist;
 import com.example.musicinfotracker.dto.Playlist;
+import com.example.musicinfotracker.dto.Track;
 import com.example.musicinfotracker.utils.PlaylistNotFoundException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -13,6 +15,8 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class PlaylistService {
@@ -47,6 +51,33 @@ public class PlaylistService {
             playlist.setPublicStatus(jsonNode.get("public").asBoolean());
             playlist.setDescription(jsonNode.get("description").asText());
             playlist.setFollowers(jsonNode.get("followers").get("total").asInt());
+
+            List<Track> playlist_tracks = new ArrayList<>();
+            for(int i = 0; i < jsonNode.get("tracks").get("items").size(); i++){
+                JsonNode trackNode = jsonNode.get("tracks").get("items").get(i).get("track");
+                Track track = new Track();
+
+                track.setId(trackNode.get("id").asText());
+                track.setImageSource(trackNode.get("album").get("images").get(0).get("url").asText());
+                track.setName(trackNode.get("name").asText());
+                track.setPreview_url(trackNode.get("preview_url").asText());
+                track.setDurationMs(trackNode.get("duration_ms").asInt());
+
+                List<Artist> trackArtists = new ArrayList<>();
+                for(int j = 0; j < trackNode.get("artists").size(); j++){
+                    JsonNode trackArtistNode = trackNode.get("artists").get(j);
+                    Artist artist = new Artist();
+
+                    artist.setId(trackArtistNode.get("id").asText());
+                    artist.setName(trackArtistNode.get("name").asText());
+
+                    trackArtists.add(artist);
+                }
+                track.setArtists(trackArtists);
+
+                playlist_tracks.add(track);
+            }
+            playlist.setTracks(playlist_tracks);
 
             return playlist;
         }
