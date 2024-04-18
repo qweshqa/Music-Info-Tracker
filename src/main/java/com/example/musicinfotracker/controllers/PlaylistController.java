1e9155f5313fd555df4780373b0e1df9a0bb3647
qweshqa <stasigrok2712@gmail.com>
2024-04-18 04:21:05 +0300

    add playlist page

--- src/main/java/com/example/musicinfotracker/controllers/PlaylistController.java
+++ src/main/java/com/example/musicinfotracker/controllers/PlaylistController.java
@@ -1,4 +1,38 @@
 package com.example.musicinfotracker.controllers;
 
+import com.example.musicinfotracker.dto.Playlist;
+import com.example.musicinfotracker.services.PlaylistService;
+import com.example.musicinfotracker.utils.PlaylistNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+@Controller
+@RequestMapping("/playlist")
 public class PlaylistController {
+    public PlaylistService playlistService;
+
+    @Autowired
+    public PlaylistController(PlaylistService playlistService){
+        this.playlistService = playlistService;
+    }
+
+    @GetMapping("/{id}")
+    public String viewPlaylist(@PathVariable("id") String id, Model model) throws IOException, InterruptedException {
+        Playlist playlist;
+
+        try{
+            playlist = playlistService.getPlaylist(id);
+        } catch (PlaylistNotFoundException ignored){
+            model.addAttribute("errorMsg", "Playlist wasn't found");
+            return "404page";
+        }
+        model.addAttribute("playlist", playlist);
+        return "playlist";
+    }
 }
--- src/main/java/com/example/musicinfotracker/services/PlaylistService.java
+++ src/main/java/com/example/musicinfotracker/services/PlaylistService.java
@@ -1,8 +1,18 @@
 package com.example.musicinfotracker.services;
 
+import com.example.musicinfotracker.dto.Playlist;
+import com.example.musicinfotracker.utils.PlaylistNotFoundException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 @Service
 public class PlaylistService {
@@ -12,5 +22,32 @@ public class PlaylistService {
     @Autowired
     public PlaylistService(@Qualifier("accessToken") String accessToken) { this.accessToken = accessToken; }
 
+    public Playlist getPlaylist(String id) throws IOException, InterruptedException{
+        String requestUrl = "https://api.spotify.com/v1/playlists/" + id;
 
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(requestUrl))
+                .header("Authorization", "Bearer " + accessToken)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if(response.statusCode() == 200){
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(response.body());
+
+            Playlist playlist = new Playlist();
+
+            playlist.setId(jsonNode.get("id").asText());
+            playlist.setName(jsonNode.get("name").asText());
+            playlist.setOwner(jsonNode.get("owner").get("display_name").asText());
+            playlist.setImageSource(jsonNode.get("images").get(0).get("url").asText());
+            playlist.setPublicStatus(jsonNode.get("public").asBoolean());
+
+            return playlist;
+        }
+        throw new PlaylistNotFoundException();
+    }
 }
